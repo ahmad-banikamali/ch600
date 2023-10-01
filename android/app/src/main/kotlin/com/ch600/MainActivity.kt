ae29618678c4b0493787dc3894a03c6d6fd33e1f
ahmad banikamali <ahmad.banikamali@gmail.com>
2023-10-01 13:40:48 +0330

    add alert for protect app in huawie phones

--- android/app/src/main/kotlin/com/ch600/MainActivity.kt
+++ android/app/src/main/kotlin/com/ch600/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.ch600
 
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.os.Process
+import java.io.IOException
+import java.lang.reflect.InvocationTargetException
 import com.ch600.sms.getSms
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -18,6 +26,7 @@ class MainActivity : FlutterFragmentActivity() {
             val messageBroadcastReceiver = MessageBroadcastReceiver();
             when (call.method) {
                 "setAlarm" -> {
+                    ifHuaweiAlert()
                     messageBroadcastReceiver.setAlarm(baseContext, call)
                     result.success("added")
                 }
@@ -39,4 +48,70 @@ class MainActivity : FlutterFragmentActivity() {
     }
 
 
+    private fun ifHuaweiAlert() {
+        val settings: SharedPreferences = getSharedPreferences("ProtectedApps",
+            Context.MODE_PRIVATE
+        )
+        val saveIfSkip = "skipProtectedAppsMessage"
+        val skipMessage = settings.getBoolean(saveIfSkip, false)
+        if (!skipMessage) {
+            val editor = settings.edit()
+            val intent = Intent()
+
+            intent.setClassName(
+                "com.huawei.systemmanager",
+                "com.huawei.systemmanager.optimize.process.ProtectActivity"
+            )
+
+            if (isCallable(intent)) {
+                AlertDialog.Builder(this)
+                    .setIcon(R.mipmap.launcher_icon)
+                    .setMessage(
+                        "این برنامه برای عملکرد درست خود باید از لیست برنامه های محافظت شده حذف شود"
+                    )
+                    .setPositiveButton("ورود به صفحه برنامه های محافظت شده") { _, _ -> huaweiProtectedApps() }
+                    .setCancelable(false)
+                    .show()
+            } else {
+                editor.putBoolean(saveIfSkip, true)
+                editor.apply()
+            }
+        }
+    }
+
+    private fun isCallable(intent: Intent): Boolean {
+        val list = packageManager.queryIntentActivities(
+            intent,
+            PackageManager.MATCH_DEFAULT_ONLY
+        )
+        return list.size > 0
+    }
+
+    private fun huaweiProtectedApps() {
+        try {
+            var cmd = "am start -n com.huawei.systemmanager/.optimize.process.ProtectActivity"
+            cmd += " --user " + getUserSerial()
+            Runtime.getRuntime().exec(cmd)
+        } catch (ignored: IOException) {
+        }
+    }
+
+
+    private fun getUserSerial(): String {
+        val userManager = getSystemService("user") ?: return ""
+        try {
+            val myUserHandleMethod =
+                Process::class.java.getMethod("myUserHandle", null)
+            val myUserHandle = myUserHandleMethod.invoke(Process::class.java, null)
+            val getSerialNumberForUser =
+                userManager.javaClass.getMethod("getSerialNumberForUser", myUserHandle.javaClass)
+            val userSerial = getSerialNumberForUser.invoke(userManager, myUserHandle) as Long
+            return userSerial.toString() ?: ""
+        } catch (ignored: NoSuchMethodException) {
+        } catch (ignored: IllegalArgumentException) {
+        } catch (ignored: InvocationTargetException) {
+        } catch (ignored: IllegalAccessException) {
+        }
+        return ""
+    }
 }
